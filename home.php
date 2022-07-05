128758b385db7cc6b591e28a73ff0642ea1571b3
maulanadaffaay <maulanadaffa@student.uns.ac.id>
2022-07-05 18:46:40 +0700

    tambah browseby + event & modif footer

--- home.php
+++ home.php
@@ -50,7 +50,7 @@
             <div class="content">
                <span>explore, discover, travel</span>
                <h3>travel arround the world</h3>
-               <a href="package.php" class="btn">discover more</a>
+               <a href="event.php" class="btn">discover more</a>
             </div>
          </div>
 
@@ -58,7 +58,7 @@
             <div class="content">
                <span>explore, discover, travel</span>
                <h3>discover the new places</h3>
-               <a href="package.php" class="btn">discover more</a>
+               <a href="event.php" class="btn">discover more</a>
             </div>
          </div>
 
@@ -66,7 +66,7 @@
             <div class="content">
                <span>explore, discover, travel</span>
                <h3>make your tour worthwhile</h3>
-               <a href="package.php" class="btn">discover more</a>
+               <a href="event.php" class="btn">discover more</a>
             </div>
          </div>
          
@@ -136,7 +136,7 @@
    <div class="content">
       <h3>about us</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita et, recusandae nobis fugit modi quibusdam ea assumenda, nulla quisquam repellat rem aliquid sequi maxime sapiente autem ipsum? Nobis, provident voluptate?</p>
-      <a href="about.php" class="btn">read more</a>
+      <a href="stories.php" class="btn">read more</a>
    </div>
 
 </section>
@@ -186,7 +186,7 @@
 
    </div>
 
-   <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+   <div class="load-more"> <a href="event.php" class="btn">load more</a> </div>
 
 </section>
 
@@ -229,9 +229,10 @@
       <div class="box">
          <h3>quick links</h3>
          <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
-         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
-         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="browseby.php"> <i class="fas fa-angle-right"></i> Browse By</a>
+         <a href="event.php"> <i class="fas fa-angle-right"></i> Event</a>
          <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+         <a href="stories.php"> <i class="fas fa-angle-right"></i> Stories</a>
       </div>
 
       <div class="box">
